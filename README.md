1fc234c1b9a86fe73548b1738b77b954f1b1fe01
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2021-11-07 23:18:28 +0100

    v1

---
+++ README.md
@@ -0,0 +1,1 @@
+# italian-caesar-decrypter
---
+++ cesare.py
@@ -0,0 +1,130 @@
+from random import randint
+class Match:
+    def __init__(self, key, phrase):
+        self.phrase = phrase
+        self.key = key
+
+    def log(self):
+        print(f"\nKey:  {self.key}")
+        print("Decrypted Phrase:")
+        print(self.phrase)
+
+def sort_by_string_length(arr, reverse=False):
+    for x in arr:
+        i = arr.index(x)
+        temp = x
+ 
+        # Insert arr[j] at its correct position
+        j = i - 1
+        while j >= 0 and len(temp) < len(arr[j]):
+            arr[j + 1] = arr[j]
+            j -= 1
+ 
+        arr[j + 1] = temp
+    if reverse:
+        arr.reverse()
+ 
+
+def decrypt(key, crypted_phrase):
+    key = int(key)
+    decrypter = []
+
+    # Sposto l'alfabeto indietro di "key"
+    for i in range(key, len(alfabeto)):
+        decrypter.append(alfabeto[i])
+
+    for i in range(0, key):
+        decrypter.append(alfabeto[i])
+
+    # decrypto in base all'alfabeto decrypter
+    decrypted = ""
+    for letter in crypted_phrase:
+        if letter == " ":
+            decrypted += " "
+        else:
+            decrypted_letter = alfabeto[decrypter.index(letter.upper())]
+            if letter.islower():
+                decrypted += decrypted_letter.lower()
+            else:
+                decrypted += decrypted_letter
+
+    return decrypted
+
+def bruteforce(crypted_phrase):
+    splitted = crypted_phrase.split(" ")
+    words = []
+    for x in splitted:
+        if len(x) > 1:
+            words.append(x)
+    if len(words) == 0:
+        print("Inserisci almeno una parola da due lettere")
+        return
+    
+    sort_by_string_length(words, True) # order by word length
+    matches = []
+    i = 0
+
+    while i < len(words) and len(matches) != 1:
+        if not matches:
+            word = words[i]
+            for j in range(0, len(alfabeto)):
+                dw = decrypt(j, word)    
+                if dw in ITALIAN_WORDS: # time complexity O(n) n = len(ITALIAN_WORDS)
+                    matches.append(j)
+        else:
+            prev_matches = matches.copy()
+            matches = []
+            word = words[i]
+            for j in prev_matches:
+                dw = decrypt(j, word)    
+                if dw in ITALIAN_WORDS: # time complexity O(n) n = len(ITALIAN_WORDS)
+                    matches.append(j)
+        i += 1
+
+    if matches:
+        valids = []
+        for key in matches:
+           match = Match(key, decrypt(key, crypted_phrase)) 
+           valids.append(match)
+        return valids
+    else: 
+        return False
+
+
+
+
+
+def statistics_sorter(el):
+    return el.count
+                
+alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+ITALIAN_WORDS = []
+
+#####################
+# Execution
+#####################
+
+
+try:
+    file = open("./bruteforce/italian.txt", "r")
+    print("Caricando il dizionario...")
+    words = file.readlines()
+    for word in words: ITALIAN_WORDS.append(word.replace("\n", "").strip())
+except FileNotFoundError:
+    print('Please provide a file with italian words named "italian.txt" and place it in the bruteforce folder in the project directory')
+
+crypted = input("Inserisci la stringa criptata [Cesare - Alfabeto completo]\n")
+
+print("\nBruteforce:")
+
+
+valids = bruteforce(crypted)
+if valids:
+    for valid in valids:
+        valid.log()
+else:
+    print("Bruteforce fallito")
+print("\n")
+
+
+
